a84714ba0f3324992349485467cd9c5bdf0ba8c2
zz cienet <zzcienet@mbp.local>
2013-12-02 17:48:32 +0800

    init SwapFace

---
+++ SwapFace/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SwapFace
+//
+//  Created by zz cienet on 12/2/13.
+//  Copyright (c) 2013 zzyunying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SwapFace/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SwapFace
+//
+//  Created by zz cienet on 12/2/13.
+//  Copyright (c) 2013 zzyunying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
